afbcfbdb49fe423be564e8e74945f15368ae137a
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-02 19:38:48 -0800

    removed API key variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
-const APIKey = "b3c89b198d75e42b324fef56894937ee"
+
 
 // const requestForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
 
 // const requestApiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=${APIKey}`
 // const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-const searchCity = document.querySelector('#searchbar')
+const searchCity = $('#searchbar')
+const formSection = $('form')
+const formmm= document.querySelector('form')
+console.log(searchCity)
+
+console.log(searchCity.val())
+
 
 function searchWeather(event) {
-    event.preventDefault()
+    event.preventDefault();
+    console.log("hello");
 
     const city = searchCity.val()
-    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b3c89b198d75e42b324fef56894937ee`
+    console.log(weatherUrl);
 
     fetch(weatherUrl).then(function(response) {
         return response.json();
@@ -20,6 +28,8 @@ function searchWeather(event) {
         console.log(data)
     })
 }
+
+function searchForecast
 
 
 // fetch(
@@ -32,5 +42,4 @@ function searchWeather(event) {
 //       console.log(data);
 //     });
 
-
-$('.search').on('submit', '#searchbar', searchWeather())
+formmm.addEventListener('submit', searchWeather)
