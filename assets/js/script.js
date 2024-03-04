2f36f0cab432a773e9bc81991ce437a09a3fa33a
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-03 21:42:33 -0800

    Updated functionalities of recent searches to be clickable to retrieve those city weather / forecast. Added aesthetic details and updated some wordings to be in more neat format.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ const formSection = $('form')
 const form= document.querySelector('form')
 const weatherData = $('#weather-data')
 const futureWeatherData = $('#future-weather-data')
+const searchArea = $('#search-area')
+const now = dayjs().format('MM/DD/YYYY')
+const recentCityContainer = $('.recent-city-container')
 
 function saveToLocalStorage(newList) {
     localStorage.setItem('saved-searches', JSON.stringify(newList))
@@ -28,16 +31,42 @@ function loadFromLocalStorage() {
     }
 }
 
+function displayRecentSearches () {
+    let savedSearches = JSON.parse(localStorage.getItem('saved-searches'))
+    
+    recentCityContainer.empty()
+
+    for (let i = 0; i < 5; i++) {
+        const recentSearch = savedSearches[i];
+        
+        
+
+        const recentCityList = $('<div>')
+        recentCityList.addClass('h4 px-2 m-4 h-auto rounded bg-secondary d-flex align-items-center uniquecity')
+        recentCityList.attr('style', 'width: 90%; min-height: 50px; cursor: pointer')
+        recentCityList.attr('id', `recent-search-button-${[i]}`)
+        recentCityList.attr('data-city-name', `${savedSearches[i]}`)
+
+        const recentCityName = $('<h4>')
+        recentCityName.attr('style', 'margin:auto')
+        recentCityName.attr('id', `recent-search-button-${[i]}`)
+        recentCityName.text(recentSearch)
+
+        recentCityContainer.append(recentCityList)
+        recentCityList.append(recentCityName)
+
+        
+    }
+}
+
 function searchWeather(event) {
     event.preventDefault();
 
+    // const clickCity = $('#recent-search-button')
+
     const city = searchCity.val()
-
-    let savedSearches = loadFromLocalStorage()
-    console.log(savedSearches)
-    savedSearches.push(city)
-    saveToLocalStorage(savedSearches)
-
+    // const cityNameByRecent = clickCity.children('h4').val()
+   
     const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b3c89b198d75e42b324fef56894937ee`
 
     fetch(weatherUrl).then(function(response) {
@@ -54,7 +83,7 @@ function searchWeather(event) {
         const cityLat = data.coord.lat
         const cityLon = data.coord.lon
 
-        if (currentWeather ==='Clouds') {
+        if (currentWeather ==='Clouds' || currentWeather ==='Haze') {
             currentWeather = '☁️'
         } else if (currentWeather ==='Clear') {
             currentWeather = '☀️'
@@ -66,10 +95,114 @@ function searchWeather(event) {
             currentWeather = '🌦️'
         } else if (currentWeather ==='Thunderstorm') {
             currentWeather = '⛈️'
+        } else if (currentWeather ==='Fog' || currentWeather==='Mist'){
+            currentWeather = '🌫️'
+        } else if (currentWeather ==='Tornado') {
+            currentWeather = '🌪️'
         }
+
+        $('#weather-data').children('div').empty()
+        $('#future-weather-data').empty()
 
         const currentCityData = $('<div>')
         currentCityData.addClass('card-container')
+        currentCityData.attr('id', 'card-data')
+        
+        weatherData.children('h2').text(`Current Weather in ${cityName} (${dayjs().format('M/D/YY')}) ${currentWeather} `)
+
+        const currentCityTemp = $('<h4>')
+        currentCityTemp.addClass('current-city-temp mx-3')
+        currentCityTemp.attr('style', 'margin-top:2rem')
+        currentCityTemp.text(`Temperature: ${currentTemp} \u2109`)
+
+        const currentCityWind = $('<h4>')
+        currentCityWind.addClass('current-city-wind mx-3')
+        currentCityWind.text(`Wind: ${currentWind} MPH`)
+
+        const currentCityHumidity = $('<h4>')
+        currentCityHumidity.addClass('current-city-humidity mx-3')
+        currentCityHumidity.text(`Humidity: ${currentHumidity}%`)
+
+        weatherData.append(currentCityData)
+        currentCityData.append(currentCityTemp)
+        currentCityData.append(currentCityWind)
+        currentCityData.append(currentCityHumidity)
+
+
+        searchForecast(cityLat, cityLon)    
+        
+        // This would return [] if there's no data in localStorage
+        let savedSearches = loadFromLocalStorage() 
+
+        // This looks for existing city name in the localStorage and if it does, it won't add.
+        // By using .unshift, we're adding the most recent search to the top.
+        const comparisonSearch = savedSearches.includes(cityName)
+        if (comparisonSearch === false) {
+            savedSearches.unshift(cityName)
+        }
+        
+        saveToLocalStorage(savedSearches)
+    
+        displayRecentSearches()
+        
+    })
+
+}
+
+function searchWeatherByClick(id) {
+    
+    let city;
+    const tableCity = recentCityContainer.children()
+    for (let i = 0; i < tableCity.length; i++) {
+        const tableCityChild = tableCity[i].attributes[2].nodeValue
+        console.log(tableCityChild);
+        const tableCityChildName = tableCity[i].attributes[3].nodeValue
+        console.log(tableCityChildName);
+        if (tableCityChild === id) {
+            city = tableCityChildName
+        }   
+    }
+    
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b3c89b198d75e42b324fef56894937ee`
+
+    fetch(weatherUrl).then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        const currentTemp = data.main.temp
+        const currentWind = data.wind.speed
+        const cityName = data.name
+        let currentWeather = data.weather[0].main
+        const currentHumidity = data.main.humidity
+        console.log(currentWeather)
+        const cityLat = data.coord.lat
+        const cityLon = data.coord.lon
+
+        if (currentWeather ==='Clouds' || currentWeather ==='Haze') {
+            currentWeather = '☁️'
+        } else if (currentWeather ==='Clear') {
+            currentWeather = '☀️'
+        } else if (currentWeather ==='Snow') {
+            currentWeather = '🌨️'
+        } else if (currentWeather ==='Rain') {
+            currentWeather = '🌧️'
+        } else if (currentWeather ==='Drizzle') {
+            currentWeather = '🌦️'
+        } else if (currentWeather ==='Thunderstorm') {
+            currentWeather = '⛈️'
+        } else if (currentWeather ==='Fog' || currentWeather==='Mist'){
+            currentWeather = '🌫️'
+        } else if (currentWeather ==='Tornado') {
+            currentWeather = '🌪️'
+        }
+
+        $('#weather-data').children('div').empty()
+        $('#future-weather-data').empty()
+
+        const currentCityData = $('<div>')
+        currentCityData.addClass('card-container')
+        currentCityData.attr('id', 'card-data')
         
         weatherData.children('h2').text(`Current Weather in ${cityName} ${currentWeather}`)
 
@@ -94,8 +227,22 @@ function searchWeather(event) {
 
         searchForecast(cityLat, cityLon)    
         
+        // This would return [] if there's no data in localStorage
+        let savedSearches = loadFromLocalStorage() 
+
+        // This looks for existing city name in the localStorage and if it does, it won't add.
+        // By using .unshift, we're adding the most recent search to the top.
+        const comparisonSearch = savedSearches.includes(cityName)
+        if (comparisonSearch === false) {
+            savedSearches.unshift(cityName)
+        }
+        
+        saveToLocalStorage(savedSearches)
+    
+        displayRecentSearches()
         
     })
+
 }
 
 function searchForecast(lat,lon) {
@@ -112,8 +259,10 @@ function searchForecast(lat,lon) {
             const futureTemp = forecastData[i].main.temp
             const futureWind = forecastData[i].wind.speed
             const futureHumidity = forecastData[i].main.humidity
+            const futureDate = dayjs(forecastData[i].dt_txt)
+            const formattedDate = futureDate.format('M/D/YY [-] h A')
 
-            if (futureWeather ==='Clouds') {
+            if (futureWeather ==='Clouds' || futureWeather ==='Haze') {
                 futureWeather = '☁️'
             } else if (futureWeather ==='Clear') {
                 futureWeather = '☀️'
@@ -125,34 +274,38 @@ function searchForecast(lat,lon) {
                 futureWeather = '🌦️'
             } else if (futureWeather ==='Thunderstorm') {
                 futureWeather = '⛈️'
+            } else if (futureWeather ==='Fog' || futureWeather==='Mist'){
+                futureWeather = '🌫️'
+            } else if (futureWeather ==='Tornado') {
+                futureWeather = '🌪️'
             }
 
             const newCard = $('<div>')
             newCard.addClass('card col-lg-2 border m-3 text-bg-primary')
-            newCard.attr('style', 'height: 300px')
+            newCard.attr('style', 'min-height: 300px')
             
             const newCardHeader = $('<div>')
             newCardHeader.addClass('card-header h3')
-            newCardHeader.text('Day 2 - need dayjs()')
+            newCardHeader.text(formattedDate)
 
             const newCardBody = $('<div>')
-            newCardBody.addClass('card-body h2 col justify-between')
+            newCardBody.addClass('card-body h2 d-flex flex-column justify-content-evenly')
 
             const newCardWeather = $('<div>')
-            newCardWeather.addClass('card-text h4')
+            newCardWeather.addClass('card-text h2')
             newCardWeather.text(futureWeather)
 
             const newCardTemp = $('<div>')
             newCardTemp.addClass('card-text h4')
-            newCardTemp.text(futureTemp)
+            newCardTemp.text(`Temp: ${futureTemp} \u2109`)
 
             const newCardWind = $('<div>')
             newCardWind.addClass('card-text h4')
-            newCardWind.text(futureWind)
+            newCardWind.text(`Wind: ${futureWind} MPH`)
 
             const newCardHumidity = $('<div>')
             newCardHumidity.addClass('card-text h4')
-            newCardHumidity.text(futureHumidity)
+            newCardHumidity.text(`Humidity: ${futureHumidity}%`)
 
             futureWeatherData.append(newCard)
             newCard.append(newCardHeader)
@@ -167,15 +320,23 @@ function searchForecast(lat,lon) {
 
 }
 
+function mouseClickValue(event) {
+    const mousePointer = event.target.id
+    console.log(mousePointer);
+    searchWeatherByClick(mousePointer)
+}
 
-// fetch(
-//     requestApiUrl
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
+displayRecentSearches()
 
 form.addEventListener('submit', searchWeather)
+
+recentCityContainer.on('click', mouseClickValue)
+
+const tableCity = recentCityContainer.children()
+const tableCityChild = tableCity[0].attributes[3].nodeValue
+
+console.log(tableCity)
+console.log(tableCityChild);
+
+
+
